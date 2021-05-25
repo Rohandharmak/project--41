5254fccb357b216fd30740a00540b020e0ee5774
Rohandharmak <71077549+Rohandharmak@users.noreply.github.com>
2021-05-25 19:40:17 +0530

    Add files via upload

---
+++ player.js
@@ -0,0 +1,53 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score=0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    getState(){
+        var PlayerscoreRef  = database.ref('playerScore');
+        PlayerscoreRef.on("value",function(data){
+           gameState = data.val();
+        })
+       
+      }
+    
+      update(score){
+        database.ref('/').update({
+         Playerscore: score
+        });
+      }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    
+}
